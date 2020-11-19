89d3464b7eaedcd625864728da6372af08ac330c
Олександр Грибинюк <oleksandrgribinuk@MacBook-Pro-Oleksandr.local>
2020-11-19 16:02:42 +0200

    Router

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -4,10 +4,10 @@ class Menu {
         const html = `
         <div class="dws-menu">
          <ul>
-           <li route="/"><a href="index"><i"></i>Головна</a></li>
-           <li route="/share"><a href="share"><i"></i>Акції</a></li>
-           <li route="/cart"><a href="cart"><i"></i>Корзина</a></li>
-           <li route="/order"><a href="order"><i "></i>Зробити замовлення</a></li>
+           <li route="/"><a href="index"><i></i>Головна</a></li>
+           <li route="/share"><a href="share"><i></i>Акції</a></li>
+           <li route="/cart"><a href="cart"><i></i>Корзина</a></li>
+           <li route="/order"><a href="order"><i></i>Зробити замовлення</a></li>
          </ul> 
       </div>
         `;
